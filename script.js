daffa2a681c65d61debbe2f04302c22c0225cdbe
0Tucho <danielivqm@gmail.com>
2024-07-25 16:19:41 -0300

    Más barrios añadidos

--- script.js
+++ script.js
@@ -19,9 +19,33 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Datos de las sedes vecinales (coordenadas ajustadas para Trelew)
     const sedes = [
-        {lat: -43.2444, lng: -65.2844, nombre: "Planta de Gas", direccion: "Juana Azurduy 419", presidente: "Johana Diaz", estado: "Normal", color: "#41ed00"},
-        {lat: -43.2660, lng: -65.3567, nombre: "Inta", direccion: "Gobernador Costa y Gaiman", presidente: "Daniel Figueroa", estado: "Normal", color: "#41ed00"},
-        {lat: -43.2667, lng: -65.3067, nombre: "San José", direccion: "Onas y Remedios de Escalada", presidente: "Esteban Collio", estado: "Normal", color: "#41ed00"},
+        {lat: -43.2444, lng: -65.2844, nombre: "Planta de Gas", direccion: "Juana Azurduy 419", presidente: "Johana Diaz", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2660, lng: -65.3567, nombre: "Inta", direccion: "Gobernador Costa y Gaiman", presidente: "Daniel Figueroa", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2667, lng: -65.3067, nombre: "San José", direccion: "Onas y Remedios de Escalada", presidente: "Esteban Collio", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2555, lng: -65.3338, nombre: "Corradi", direccion: "Ecuador y Simon de Alcazabar", presidente: "Camila Sepulveda", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2469, lng: -65.3255, nombre: "Los Aromos", direccion: "Los Arrayanes, entre Las Araucarias y Las Acacias", presidente: "Ruben Lencina", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2602, lng: -65.3321, nombre: "Malvinas", direccion: "Juan de la Piedra 325", presidente: "Claudia Cañumil", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2594, lng: -65.3472, nombre: "Menfa", direccion: "26 de Noviembre y Viedman", presidente: "Eduardo Singler", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2405, lng: -65.2877, nombre: "Norte", direccion: "Las Heras 78", presidente: "Veronica Rivera", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2465, lng: -65.3181, nombre: "Oeste", direccion: "Pje. Córdoba y Fuerte San José", presidente: "Pedro Jaramillo", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2431, lng: -65.3038, nombre: "Don Bosco", direccion: "Pje. Córdoba y Fuerte San José", presidente: "Pablo Monteros", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2732, lng: -65.3057, nombre: "Etchepare", direccion: "Scalabrini Ortiz 1753", presidente: "Luis Treuquil", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2552, lng: -65.3250, nombre: "Progreso", direccion: "Entre Rios 146", presidente: "Andres Obregon", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2451, lng: -65.3135, nombre: "Pte. Perón", direccion: "Rivadavia 419", presidente: "Florencia Beizaga", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2554, lng: -65.2805, nombre: "Santa Catalina", direccion: "Maria Humprey 1169", presidente: "Alejandra Vazquez", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2583, lng: -65.3214, nombre: "Sarmiento", direccion: "Uruguay 470", presidente: "Mario Diaz", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2670, lng: -65.3101, nombre: "Santa Mónica", direccion: "Emilio Frey 1280", presidente: "Gisell Aguirre", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2369, lng: -65.3022, nombre: "Tiro Federal", direccion: "José Montes 811", presidente: "Bernardino Albial", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2466, lng: -65.3023, nombre: "Unión", direccion: "Alem 30", presidente: "Stella Maris Ferreyra", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2616, lng: -65.2896, nombre: "Villa Italia", direccion: "Maria Humpreys 202", presidente: "Fernando Sanchez", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2701, lng: -65.3097, nombre: "Illia", direccion: "Scalabrini Ortiz y Fray Luis Beltrán", presidente: "Andres San Martin", estado: "Activo", color: "#41ed00"},
+        {lat: -43.2493, lng: -65.3268, nombre: "Constitución", direccion: "Sin Sede", presidente: "Gastón Espiace", estado: "Utilizando una biblioteca provisoriamente", color: "#eaef00"},
+        {lat: -43.2606, lng: -65.3563, nombre: "Moreira 1/2/3/4", direccion: "Sin Sede", presidente: "Marcela Rechimon", estado: "Se encuentra activa pero sin talleres y demás actividades, por no tener espacio físico", color: "#eaef00"},
+        {lat: -43.2758, lng: -65.2949, nombre: "Los Sauces", direccion: "Sin Sede", presidente: "Juan José Galtieri", estado: "Se encuentra activa pero sin talleres y demás actividades, por no tener espacio físico", color: "#eaef00"},
+        {lat: -43.2487, lng: -65.2871, nombre: "Guayra", direccion: "Sin Sede", presidente: "Julieta Lewis", estado: "Se encuentra activa pero sin talleres y demás actividades, por no tener espacio físico", color: "#eaef00"},
+        {lat: -43.2810, lng: -65.2712, nombre: "Los Paraísos", direccion: "Sin Sede", presidente: "Jorge Omar Subiela", estado: "Se encuentra activa pero sin talleres y demás actividades, por no tener espacio físico", color: "#eaef00"},
+        {lat: -43.2668, lng: -65.2956, nombre: "San David", direccion: "Juan Manuel de Rosas 1748", presidente: "Teresa Mackenzie", estado: "Se reactiva proximamente", color: "#eaef00"},
+        {lat: -43.2411, lng: -65.3125, nombre: "San Martin", direccion: "Sin Sede", presidente: "Lucio Castillo", estado: "Inactivo", color: "#f44336"},
         // Añadir más sedes aquí
     ];
 
